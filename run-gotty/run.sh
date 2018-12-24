c6eeee85e628592d582e5516a843e417f93db0cf
Marcin Tojek <tojek.m@gmail.com>
2018-12-24 11:50:19 +0100

    Use tarballs

--- run-gotty/run.sh
+++ run-gotty/run.sh
@@ -4,8 +4,8 @@ curl -s -L https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_a
 tar xzf /tmp/gotty.tar.gz -C /tmp/ && rm /tmp/gotty.tar.gz
 /tmp/gotty -w --port 7591 /bin/bash &
 
-curl -s -L https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o /tmp/ngrok.zip
-unzip /tmp/ngrok.zip -d /tmp && rm /tmp/ngrok.zip
+curl -s -L https://bin.equinox.io/a/iVLSfdAz1X4/ngrok-stable-linux-amd64.tar.gz -o /tmp/ngrok.tar.gz
+tar xzf /tmp/ngrok.tar.gz -C /tmp/ && rm /tmp/ngrok.tar.gz
 /tmp/ngrok http 7591 &
 sleep 5
 
