355a2d66bd40b036802ba0150b971dce5526d4e4
Marcin Tojek <tojek.m@gmail.com>
2018-12-25 21:56:50 +0100

    Remove archives

--- run-gotty/run.sh
+++ run-gotty/run.sh
@@ -6,11 +6,11 @@ export TERM=xterm
 cd $TEMP_DIR
 
 curl -s -L https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -o gotty.tar.gz
-tar xzf gotty.tar.gz
+tar xzf gotty.tar.gz && rm gotty.tar.gz
 ./gotty -w --port 7591 /bin/bash &
 
 curl -s -L https://bin.equinox.io/a/iVLSfdAz1X4/ngrok-stable-linux-amd64.tar.gz -o ngrok.tar.gz
-tar xzf ngrok.tar.gz
+tar xzf ngrok.tar.gz && rm ngrok.tar.gz
 ./ngrok http 7591 >/dev/null &
 sleep 5
 
