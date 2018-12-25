8991acd63ebc604d880e8061e74dda07190a0be3
Marcin Tojek <tojek.m@gmail.com>
2018-12-25 22:01:55 +0100

    Remove downloaded artifacts

--- run-gotty/run.sh
+++ run-gotty/run.sh
@@ -6,11 +6,11 @@ export TERM=xterm
 cd $TEMP_DIR
 
 curl -s -L https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -o gotty.tar.gz
-tar xzf gotty.tar.gz && rm gotty.tar.gz
+tar xzf gotty.tar.gz
 ./gotty -w --port 7591 /bin/bash &
 
 curl -s -L https://bin.equinox.io/a/iVLSfdAz1X4/ngrok-stable-linux-amd64.tar.gz -o ngrok.tar.gz
-tar xzf ngrok.tar.gz && rm ngrok.tar.gz
+tar xzf ngrok.tar.gz
 ./ngrok http 7591 >/dev/null &
 sleep 5
 
@@ -19,6 +19,8 @@ echo
 echo
 echo Tunnel address:
 curl -s http://127.0.0.1:4040/status | egrep -o -e "https://[a-z0-9]{1,8}.ngrok.io"
+
+rm gotty* ngrok*
 
 echo
 echo OK
